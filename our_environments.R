d2f293d6d3138a402092bff650c8caa05ae82275
ego5721ist <32357468+ego5721ist@users.noreply.github.com>
2017-10-10 04:43:16 +0800

    Update our_environments.R

--- our_environments.R
+++ our_environments.R
@@ -2,4 +2,5 @@ list(
   Ingrid = "x86_64-w64-mingw32/x64 (64-bit)",
   CHEN_FANG_JIAN="x86_64-w64-mingw32/x64 (64-bit)",
   Diane="x86_64-apple-darwin15.6.0 (64-bit)",
+  Dylan="x86_64-w64-mingw32/x64 (64-bit)",
 )
