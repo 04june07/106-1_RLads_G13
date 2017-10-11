e61214c1e050cea20f8239a0b1b97022a3216ded
ProgrammingIdiot <32357473+ProgrammingIdiot@users.noreply.github.com>
2017-10-11 10:49:05 +0800

    Update our_environments.R

--- our_environments.R
+++ our_environments.R
@@ -3,4 +3,5 @@ list(
   CHEN_FANG_JIAN="x86_64-w64-mingw32/x64 (64-bit)",
   Diane="x86_64-apple-darwin15.6.0 (64-bit)",
   Dylan="x86_64-w64-mingw32/x64 (64-bit)",
+  Gary="x86_64-apple-darwin13.4.0 (64-bit)",
 )
